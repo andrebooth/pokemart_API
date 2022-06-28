5f5a9c5ae614a631c35f68b74d42b3a669ce430a
Andre Booth <DreLeRoy@MacBook-Pro.local>
2022-06-28 09:28:44 -0700

    API GET and POST request work

--- pokemart/pokemart/urls.py
+++ pokemart/pokemart/urls.py
@@ -19,5 +19,5 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pokemart_items/', views.pokemart_list)
+    path('pokemart_list/', views.pokemart_list)
 ]
--- pokemart/pokemart/views.py
+++ pokemart/pokemart/views.py
@@ -3,12 +3,25 @@
 from django.http import JsonResponse
 from .models import Pokemart
 from .serializer import PokemartSerialzer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 
+@api_view(['GET', 'POST'])
+#GET: Function that will takr a GET request for all items 
 def pokemart_list(request):
     #get all the drinks from drink class in models
     #serialize them from serializer class going to take the pokemart list, and to serialize all of them
     #return json
-   items = Pokemart.objects.all()
-   serializer = PokemartSerialzer(items, many=True)
-   return JsonResponse(serializer.data, safe=False)
+
+    if request.method == 'GET':
+        items = Pokemart.objects.all()
+        serializer = PokemartSerialzer(items, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    if request.method == 'POST':
+        serializer = PokemartSerialzer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
